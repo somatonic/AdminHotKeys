b51093e76db1c0b42fa30481d61e011d1c2830ca
somatonic <philipp@urlich,ch>
2012-07-18 02:25:44 +0200

    fixed function scope for FF

--- AdminHotKeys.js
+++ AdminHotKeys.js
@@ -3,13 +3,15 @@
 $(function(){
 	// save hot key for all edit screens
 	if($('button[id*="submit"]').length > 0){
+		function savePage(e){ $('button[id*="submit"]').trigger('click'); };
 		$(document).bind('keydown', config.AdminHotKeys.hk_save, savePage);
-		function savePage(e){ $('button[id*="submit"]').trigger("click"); };
+
 	}
 	// add hot key for "add new"
 	if($('a[href*="add"]').length > 0){
+		function addNew(e){ $('a[href*="add"] button').trigger('click'); };
 		$(document).bind('keydown', config.AdminHotKeys.hk_addnew, addNew);
-		function addNew(e){ $('a[href*="add"] button').trigger("click"); };
+
 	}
 
 });
