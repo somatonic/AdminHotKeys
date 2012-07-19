e26091864e3c6239f5dd364a580627746e40aac6
somatonic <philipp@urlich,ch>
2012-07-19 02:44:50 +0200

    added view page "ctrl-v", added label link on autcomplete as shortcut to template/field list

--- AdminHotKeys.js
+++ AdminHotKeys.js
@@ -8,14 +8,19 @@ $(function(){
 	if($('button[id*="submit"]').length > 0){
 		function savePage(e){ $('button[id*="submit"]').trigger('click'); };
 		$(document).bind('keydown', hkconf.hk_save, savePage);
-
 	}
 	// add hot key for "add new"
 	if($('a[href*="add"]').length > 0){
 		function addNew(e){ $('a[href*="add"] button').trigger('click'); };
 		$(document).bind('keydown', hkconf.hk_addnew, addNew);
-
 	}
+	// add hot key for "view" on page edit screen
+	if($('a[id="_ProcessPageEditView"]').length > 0){
+		function viewPage(){ window.location.href = $('a[id="_ProcessPageEditView"]').attr('href'); };
+		$(document).bind('keydown', hkconf.hk_view , viewPage);
+	}
+
+
 	// add hot key for /setup/
 	function openSetup(){ window.location.href = $('#topnav a[href*="/setup"]').attr('href'); };
 	$(document).bind('keydown', hkconf.hk_opensetup , openSetup);
@@ -38,12 +43,12 @@ $(function(){
 	// add hot key for template,field autocomplete edit
 	function openHKTemplateList(){
 		$('#hk_list').remove();
-		prepareHKContainer('Edit Template:',hkconf.hk_pwtemplates, 'setup/template/edit');
+		prepareHKContainer( hkconf.hk_pwtemplates, 'Edit <a href="'+config.urls.admin+'setup/template/">Template</a>:', 'setup/template/edit' );
 		$('#hk_list').fadeIn().find('input:first').focus();
 	};
 	function openHKFieldList(){
 		$('#hk_list').remove();
-		prepareHKContainer('Edit Field:',hkconf.hk_pwfields, 'setup/field/edit');
+		prepareHKContainer( hkconf.hk_pwfields, 'Edit <a href="'+config.urls.admin+'setup/field/">Field</a>:', 'setup/field/edit' );
 		$('#hk_list').fadeIn().find('input:first').focus();
 	};
 
@@ -62,14 +67,19 @@ $(function(){
 
 });
 
-var prepareHKContainer = function(label, tags, url) {
-	var $items = $('<div id="hk_list" class="hk_list ui-widget"><label>'+label+' </label><input id="hk_items"></div>');
+/**
+ * Create an autcomplete search box
+ * tags: the objects array|object
+ * label to show string|html
+ * url for the edit page
+ */
+var prepareHKContainer = function(tags, label, url) {
+	var $items = $('<div id="hk_list" class="hk_list ui-widget ui-widget-content"><label>' + label + ' </label><input id="hk_items"></div>');
 	var hk_taglist = [];
 	for(var tname in tags){
+		// create static array obects for autocomplete search
 		 hk_taglist.push({label: tname, value: tags[tname]});
-		//$tmpls.append("<a href='" + config.urls.admin + "setup/template/edit?id=" + hkconf.hk_pwtemplates[tname] + "'>" + tname + "</a>");
 	}
-	var wheight = $(window).height()/2;
 	$('body').append($items.hide());
 	$('#hk_items').autocomplete({
 		source: hk_taglist,
@@ -80,6 +90,8 @@ var prepareHKContainer = function(label, tags, url) {
 			window.location = config.urls.admin + url + "?id=" + ui.item.value;
 			return false;
 		}
-	}).find('.ui-autocomplete.ui-menu').css({height: wheight + "px"});
-
+	})
+	// set max height based on screen size
+	var wheight = $(window).height()/100 * 73;
+	$('body').find('.ui-menu').css({'maxHeight': wheight + "px"});
 };
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProcessWire Hot Keys Module v0.0.5
+# ProcessWire Hot Keys Module v0.0.6
 
 This module adds hot key functionality to ProcessWire's backend. [ProcessWire Forum thread](http://processwire.com/talk/topic/1524-admin-hot-keys/)
 
@@ -8,6 +8,7 @@ It uses a jQuery hotkeys plugin from here: [Hotkeys Github](https://github.com/j
 
 * Save : `ctrl+s`
 * Add New : `ctrl+n`
+* View Page : `ctrl+v`
 * Open Templates (autocomplete) : `ctrl+shift+t`
 * Open Fields (autocomplete) : `ctrl+shift+f`
 * Goto Pages : `ctrl+shift+p`
@@ -16,3 +17,5 @@ It uses a jQuery hotkeys plugin from here: [Hotkeys Github](https://github.com/j
 * Goto Access : `ctrl+shift+a`
 
 It can be configured to your preference in the module settings.
+
+__Tipp__: If you open up templates or fields autocomplete search `ctrl+shift+f|t` press `shift+tab` to focus on the link in the label previous to the search input box. Now hit enter to get right to the template or field screen.
